59c19b95cbd3c407d01ab4dfe06fac3013621af7
Unknown <gaborpenzinger94@gmail.com>
2018-10-07 16:31:36 +0200

    mcp2515_init()

--- src/MCP2515.c
+++ src/MCP2515.c
@@ -1,3 +1,12 @@
+void mcp2515_init(uint8_t mode)
+{
+    uint8_t value;
+    uint8_t mask = 0b011100000;
+    initSPI();
+    mcp2515_reset();
+    mcp2515_bit_modify(MCP_CANCTRL,mask,mode); //ex. mcp2515_bit_modify(MCP_CANCTRL,mask, MODE_LOOPBACK);
+    
+}
 
 
 uint8_t mcp2515_read(uint8_t address)
