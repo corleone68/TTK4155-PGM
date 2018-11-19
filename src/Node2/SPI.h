ff0d94de77d981a97869f56ec3ebb72ecb03c80a
Gabor7697 <37667063+Gabor7697@users.noreply.github.com>
2018-11-19 20:18:41 +0100

    Update SPI.h

--- src/Node2/SPI.h
+++ src/Node2/SPI.h
@@ -3,13 +3,17 @@
 #define SPI_H_
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
+
+#include <avr/io.h>
+
+#define SLAVE_SELECT PORTB &= ~(1 << PB7)
+#define SLAVE_DESELECT PORTB |= (1 << PB7)
+
+void initSPI(void);
+void transmitSPI(uint8_t data);
+uint8_t receiveSPI(uint8_t data);
 
 
-
-char SPI_Read(void);
-void SPI_Send(char cData);
-void SPI_MasterInit(void);
 
 
 
