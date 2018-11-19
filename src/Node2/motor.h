48bd76a132bae350362b7cede3481de148d7032f
corleone68 <35456504+corleone68@users.noreply.github.com>
2018-11-19 20:02:39 +0100

    Update motor.h

--- src/Node2/motor.h
+++ src/Node2/motor.h
@@ -8,20 +8,19 @@
 #define F_CPU 16000000
 #include <util/delay.h>
 #include "dac.h"
-//#include "CAN.h"
 
 typedef enum {LEFT, RIGHT} motor_direction_t;
 
-void motor_init();
-void motor_set_dir(motor_direction_t direction);
-void motor_set_speed(int8_t speed);
-void motor_set_speed_PID(uint8_t speed);
+void initMotor();
+void setDirection(motor_direction_t direction);
+void setSpeed(int8_t speed);
+void PIDcontroller(uint8_t speed);
 void motor_move(int16_t rot_diff, uint8_t power);
-uint16_t motor_encoder_read(void);
-void motor_reset_encoder();
+uint16_t readEncoder(void);
+void resetEncoder();
 
 
 
 
 
-#endif /* MOTOR_H_ */
+#endif 
