a5ec4c0e91081961d22313a2854b2d96c1b19c93
Gabor7697 <37667063+Gabor7697@users.noreply.github.com>
2018-11-19 20:26:21 +0100

    Update adc.c

--- src/Node2/adc.c
+++ src/Node2/adc.c
@@ -1,54 +1,18 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/delay.h>
 #include "adc.h"
 
 
-//***************************************************************
-//	Initialize adc												*
-//***************************************************************
-void adc_init(){
-	
-	// Set adc port as input
-	DDRF &= ~(1 << PF0);
-	
-	// Enable ADC conversion complete interrupt
-	ADCSRA	|= (1 << ADIE);
-	
-	// Set external ref
-	ADMUX	&= ~(1 << REFS1);
-	ADMUX	|= (1 << REFS0);
-	
-	// Set prescaler - division factor 128
-	ADCSRA	|= (1 << ADPS0)|(1 << ADPS1)|(1 << ADPS2);
-	
-	// Enable and start first conversion
-	ADCSRA	|= (1 << ADEN);
+void initADC(void){
+	ADMUX	|= (1 << REFS0); //reference voltage on AVCC 
+	ADCSRA	|= (1 << ADPS2)|(1 << ADPS1); //ADC clock prescaler 64
+	ADCSRA	|= (1 << ADEN); //enable ADC
+    DDRF &= ~(1 << PF0); //set as A0 as input
 }
 
-
-//***************************************************************
-//	Read from adc channel										*
-//***************************************************************
-uint16_t adc_read(uint8_t channel){
+uint16_t analogRead(uint8_t ch){
 	
-	// Add muxing between adc channels
-	ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
-	
-	// Single conversion mode
-	ADCSRA |= (1<<ADSC);
-	
-	// Wait for ADC conversion to be completed
-	while(ADCSRA&(1<<ADSC));
+	ADMUX = (ADMUX & 0xF0) | (ch & 0x0F);//set channel
+	ADCSRA |= (1<<ADSC); //start ADC conversion
+	loop_until_bit_is_clear(ADCSRA,ADCS); //wait until done
 	return ADC;
-}
-
-
-//***************************************************************
-//	Interrupt vector for ADC									*
-//***************************************************************
-ISR(ADC_vect){
-	
-	//ADCSRA &= ~(1 << ADIF);
-	//ADCSRA	|= (1 << ADSC);
 }
