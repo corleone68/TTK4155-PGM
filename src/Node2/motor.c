ff4f56acb84326c29c0bccbc3483cb3d890c17aa
corleone68 <35456504+corleone68@users.noreply.github.com>
2018-11-19 19:54:31 +0100

    Update motor.c

--- src/Node2/motor.c
+++ src/Node2/motor.c
@@ -3,38 +3,24 @@
 
 
 
-void motor_init(){
+void init_motor(){
 	
-	
-	// Enable motor
-	DDRH |= (1 << PH4);
+	DDRH |= (1 << PH4); // Enable motor
 	
 	DDRH |= (1 << PH5); // set encoder enable to out
 	
-	 // set encoder read to in
+	DDRH |= (1 << PH1); // Set direction pin to output
 	
-	// Set direction pin to output
-	DDRH |= (1 << PH1);
+	DDRH |= (1 << PH5); //PB5 // Enable control for output enable. Remember: active low - !OE
 	
-	// Enable control for output enable. Remember: active low - !OE
-	DDRH |= (1 << PH5); //PB5
-	
-	// Enable control for selection pin: SEL
-	DDRH |= (1 << PH3);
+	DDRH |= (1 << PH3); // Enable control for selection pin: SEL
 	
 	// Enable control for Reset pin: RST
 	DDRH |= (1 << PH6);
 	PORTH |= (1 << PH4);
-	
 	PORTH |= (1 << PH6);
-
-	// Reset encoder
-	//motor_reset_encoder();
 	
-	// Set data bits to input:
-	//DDRK = 0x00;
-	//Set !OE low to enable output of encoder.
-	PORTH &= ~(1 << PH5);
+	PORTH &= ~(1 << PH5); //Set !OE low to enable output of encoder.
 	
 	
 }
@@ -74,23 +60,15 @@ void motor_reset_encoder() {
 
 uint16_t motor_encoder_read(void){
 
-	
-	//Set SEL low to get high byte
-	PORTH &= ~(1 << PH3);
+	PORTH &= ~(1 << PH3); //Set SEL low to get high byte
 	_delay_us(20);
 	
-	//Read LSB
-	uint8_t high = PINK; 
+	uint8_t high = PINK; //Read LSB
 	
-	//Set SEL high to get low byte
-	PORTH |= (1 << PH3);
+	PORTH |= (1 << PH3); //Set SEL high to get low byte
 	_delay_us(20);
 	
-	//Read MSB
-	uint8_t low = PINK;
-
-	//Set !OE high to disable output of encoder
-	//PORTH |= (1 << PH5);
+	uint8_t low = PINK; //Read MSB
 	
 	uint16_t encoder_value =  (high << 8) | low;
 	
