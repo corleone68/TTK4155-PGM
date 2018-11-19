d6d401977fe9fa19f0470cb89a2d02dfffde190e
corleone68 <35456504+corleone68@users.noreply.github.com>
2018-11-19 20:09:58 +0100

    Update motor.h

--- src/Node2/motor.h
+++ src/Node2/motor.h
@@ -9,10 +9,10 @@
 #include <util/delay.h>
 #include "dac.h"
 
-typedef enum {LEFT, RIGHT} motor_direction_t;
+typedef enum {LEFT, RIGHT} dir;
 
 void initMotor();
-void setDirection(motor_direction_t direction);
+void setDirection(dir direction);
 void setSpeed(int8_t speed);
 void PIDcontroller(uint8_t speed);
 uint16_t readEncoder(void);
