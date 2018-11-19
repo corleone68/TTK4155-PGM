8c0eaca750f27369f69265dee1a610d36d23b192
Gabor7697 <37667063+Gabor7697@users.noreply.github.com>
2018-11-19 20:24:27 +0100

    Update CAN.c

--- src/Node2/CAN.c
+++ src/Node2/CAN.c
@@ -1,44 +1,43 @@
 
 #include "CAN.h"
 
-void CAN_init(uint8_t mode){
+void can_init(uint8_t mode){
 	mcp2515_init(mode);
 
-	cli(); // Disable global interrupts
+	cli(); // disable interrupts
 	
 	//DDRE &= ~(1<<PD2);    // PE4 
 	
-	EIMSK |= (1 << INT2); // Enable interrupt 2, arduino PIN 19 
+	EIMSK |= (1 << INT2); // enable interrupt 2, arduino PIN 19 
 	
-	EIFR |= (1 << INTF2); // Clear intrerrupt flag 2
+	EIFR |= (1 << INTF2); // clear intrerrupt flag 2
 	
 	EICRA |= (1<<ISC21);// falling edge
 
-	sei(); // Enable global interrupts
+	sei(); // Enable interrupts
 	
 }
-void CAN_message_send(CAN_message* msg){
+
+void can_message_send(can_message* msg){
 
 	mcp2515_write(MCP_TXB0SIDH, (msg->id) >> 3); // Set message ID high bits
-	mcp2515_write(MCP_TXB0SIDL, (msg->id) << 5); // Set message ID low bits 
+	mcp2515_write(MCP_TXB0SIDL, (msg->id) << 5); // Set message ID low bits
 	mcp2515_write(MCP_TXB0DLC, (msg->length)); // Set data length
-	
+
 	for (uint8_t i = 0; i < msg->length; i++){
 		mcp2515_write(MCP_TXB0D0+i, msg->data[i]); // Set data bits
 	}
-	mcp2515_request_to_send(MCP_RTS_TX0); // Sends data to TX0 buffer 
+	mcp2515_request_to_send(MCP_RTS_TX0); // Sends data to TX0 buffer
 }
 
-uint8_t CAN_receive(CAN_message* msg){
+uint8_t can_data_receive(can_message* msg){
 	if (CAN_int_flag){
-		uint8_t IDHI = mcp2515_read(MCP_RXB0SIDH); // Read message ID high bits
-		uint8_t IDLO = mcp2515_read(MCP_RXB0SIDL); // Read message ID low bits
-		msg->id = (IDHI << 3| IDLO >> 5); // Combine to complete message ID
-		
+		msg->id = (mcp2515_read(MCP_RXB0SIDH) << 3| mcp2515_read(MCP_RXB0SIDL) >> 5); // Combine to complete message ID
+
 		uint8_t length = mcp2515_read(MCP_RXB0DLC); // Read message data length
 		msg->length = length;
-	
-	
+
+
 		for (uint8_t i = 0; i < msg->length; i++){
 			msg->data[i] = mcp2515_read(MCP_RXB0D0+i);  // Read data bits
 		}
@@ -48,7 +47,6 @@ uint8_t CAN_receive(CAN_message* msg){
 	}
 	return 0;
 }
-
 
 ISR(INT2_vect){
 	CAN_int_flag = 1;
